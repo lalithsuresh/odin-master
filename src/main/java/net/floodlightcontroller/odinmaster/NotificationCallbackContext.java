562bf16b22dd8762a52288ea5ee2f46e072fb0b2
Lalith Suresh <suresh.lalith@gmail.com>
2012-07-10 14:39:12 +0200

    Make NotificationCallbackContext immutable

--- src/main/java/net/floodlightcontroller/odinmaster/NotificationCallbackContext.java
+++ src/main/java/net/floodlightcontroller/odinmaster/NotificationCallbackContext.java
@@ -3,7 +3,13 @@ package net.floodlightcontroller.odinmaster;
 import net.floodlightcontroller.util.MACAddress;
 
 public class NotificationCallbackContext {
-	public MACAddress clientHwAddress;
-	public IOdinAgent agent;
-	public long value;
+	public final MACAddress clientHwAddress;
+	public final IOdinAgent agent;
+	public final long value;
+	
+	public NotificationCallbackContext(final MACAddress clientHwAddress, final IOdinAgent agent, final long value) {
+		this.clientHwAddress = clientHwAddress;
+		this.agent = agent;
+		this.value = value;
+	}
 }
